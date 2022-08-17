fecf6172a4476400a4598c508f422404adad1032
Muhammad Muslih W <muslihwaskito@gmail.com>
2022-08-17 21:04:15 +0700

    initial

---
+++ js/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5c34629a85658b4d.css","static/chunks/pages/index-dc179dca00f4e75d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
